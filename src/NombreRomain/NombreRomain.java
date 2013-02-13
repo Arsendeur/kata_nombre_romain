bf44621a7fbcd6ed4c591297a53ee4b840e3d4cc
arsendeur <arsendeur@gmail.com>
2013-02-13 11:33:23 +0100

    premier test au vert (peutAfficher1)

---
+++ src/NombreRomain/NombreRomain.java
@@ -0,0 +1,16 @@
+package NombreRomain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arsendeur
+ * Date: 13/02/13
+ * Time: 11:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class NombreRomain {
+
+    public int  RomainToNombre(String nombreRomainAConvertir)
+    {
+        return 1;
+    }
+}
---
+++ src/test/testNombreRomain.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import NombreRomain.NombreRomain;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arsendeur
+ * Date: 13/02/13
+ * Time: 10:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class testNombreRomain {
+
+    @Test
+    public  void peutAfficher1()
+    {
+        NombreRomain unNombreRomain = new NombreRomain();
+
+        assertEquals(unNombreRomain.RomainToNombre("I"), 1);
+    }
+}
